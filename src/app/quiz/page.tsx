e234c0e749aa4f36f7d23824a13e916001501882
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-10-09 16:42:01 +0100

    Update: testing topics endpoint

--- src/app/quiz/page.tsx
+++ src/app/quiz/page.tsx
@@ -1,27 +1,44 @@
 export default async function QuizPage() {
   // button generate quiz, fetch internal api post req
-  const res = await fetch("http://localhost:3000/api/quizzes", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ topic: "Arrays" }),
-  });
-  const { data } = await res.json();
-  const quiz = JSON.parse(data);
 
-  const optionElements = quiz.options.map((element: string, index: number) => {
-    return (
-      <div key={index}>
-        <p>{element}</p>
-      </div>
-    );
-  });
+  const res = await fetch("http://localhost:3000/api/topics");
+  console.log(res);
+
+  if (!res.ok) {
+    const { message } = await res.json();
+    const displayMessage =
+      res.status === 500
+        ? "Something went wrong on our end. Please try again"
+        : message || "Unexpected error.";
+    throw new Error(displayMessage);
+  }
+
+  const { data } = await res.json();
+
+  console.log(data);
+
+  // const res = await fetch("http://localhost:3000/api/quizzes", {
+  //   method: "POST",
+  //   headers: { "Content-Type": "application/json" },
+  //   body: JSON.stringify({ topic: "Arrays" }),
+  // });
+  // const { data } = await res.json();
+  // const quiz = JSON.parse(data);
+
+  // const optionElements = quiz.options.map((element: string, index: number) => {
+  //   return (
+  //     <div key={index}>
+  //       <p>{element}</p>
+  //     </div>
+  //   );
+  // });
 
   return (
     <>
-      <h1>{quiz.question}</h1>
+      {/* <h1>{quiz.question}</h1>
       {optionElements}
       <p>{quiz.correct_answer}</p>
-      <p>{quiz.doc_link}</p>
+      <p>{quiz.doc_link}</p> */}
     </>
   );
 }
